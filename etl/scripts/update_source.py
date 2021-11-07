bfdd176d80b8b25ca707059e44ab53fee79d7eda
semio <semio@posteo.net>
2021-11-07 15:23:47 +0800

    version number also need update

--- etl/scripts/update_source.py
+++ etl/scripts/update_source.py
@@ -11,7 +11,7 @@ import pandas as pd
 from ddf_utils.factory.common import download
 
 
-VERSION = 2020
+VERSION = 2021
 PAGE = 'https://www.icos-cp.eu/science-and-impact/global-carbon-budget/2021'
 ARCHIVE_PAGE = 'https://www.globalcarbonproject.org/carbonbudget/archive.htm'
 SOURCE_FILE_DIR = '../source/'
